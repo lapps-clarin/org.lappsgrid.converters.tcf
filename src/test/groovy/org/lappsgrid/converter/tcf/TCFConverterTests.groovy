1e312bff7b46cbe0cc33bad39e4ae8d236086d40
Keith Suderman <suderman@cs.vassar.edu>
2017-03-07 10:48:26 +0100

    Added unit tests.

--- src/test/groovy/org/lappsgrid/converter/tcf/TCFConverterTests.groovy
+++ src/test/groovy/org/lappsgrid/converter/tcf/TCFConverterTests.groovy
@@ -1,12 +1,13 @@
 package org.lappsgrid.converter.tcf
 
-import eu.clarin.weblicht.wlfxb.io.WLDObjector
-import eu.clarin.weblicht.wlfxb.tc.api.TextCorpus
-import eu.clarin.weblicht.wlfxb.tc.api.TextLayer
-import eu.clarin.weblicht.wlfxb.xb.WLData
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.lappsgrid.serialization.Data
+import org.lappsgrid.serialization.lif.Annotation
+
+import static org.junit.Assert.*
+
 import org.lappsgrid.discriminator.Discriminators
 import org.lappsgrid.serialization.lif.Container
 import org.lappsgrid.serialization.lif.View
@@ -17,31 +18,30 @@ import org.lappsgrid.serialization.lif.View
  */
 class TCFConverterTests {
 
+    Data data
     Container container
 
     @Before
     void setup() {
-        InputStream stream = this.class.classLoader.getResourceAsStream("/karen-flew.xml")
-        if (!stream) {
-            throw new IOException("Unable to load test file.")
-        }
-        WLDObjector objector = new WLDObjector()
-        WLData data = objector.read(stream)
-
-        TextCorpus corpus = data.textCorpus
-        TextLayer textLayer = corpus.textLayer
-        if (!textLayer) {
-            throw new ConversionException("No text layer in TCF document.")
-        }
-
-        container = new Container()
-        container.text = textLayer.getText()
-        container.language = corpus.getLanguage()
+        InputStream stream = this.class.classLoader.getResourceAsStream("karen-flew.xml")
+        assert stream != null
+        data = new TCFConverter().convert(stream)
+        container = data.payload
     }
 
     @After
     void teardown() {
         container = null
+    }
+
+    String extractText(Annotation a) {
+        return container.text.substring((int)a.start, (int)a.end)
+    }
+
+    @Test
+    void testLanguageAndText() {
+        assertTrue 'en' == container.language
+        assertTrue 'Karen flew to New York.' == container.text
     }
 
     @Test
@@ -50,15 +50,37 @@ class TCFConverterTests {
         assertTrue 1 == views.size()
         assertTrue 6 == views[0].annotations.size()
 
+        List<Annotation> annotations = views[0].annotations
+        assertEquals 'Karen', extractText(annotations[0])
+        assertEquals 'flew', extractText(annotations[1])
+        assertEquals 'to', extractText(annotations[2])
+        assertEquals 'New', extractText(annotations[3])
+        assertEquals 'York', extractText(annotations[4])
+        assertEquals '.', extractText(annotations[5])
     }
 
     @Test
     void testLemmas() {
+        List<View> views = container.findViewsThatContain(Discriminators.Uri.LEMMA)
+        assertTrue 1 == views.size()
+        assertTrue 6 == views[0].annotations.size()
 
+        List<Annotation> annotations = views[0].annotations
+        assertEquals 'karen', annotations[0].features.lemma
+        assertEquals 'fly', annotations[1].features.lemma
+        assertEquals 'to', annotations[2].features.lemma
+        assertEquals 'new', annotations[3].features.lemma
+        assertEquals 'york', annotations[4].features.lemma
+        assertEquals '.', annotations[5].features.lemma
     }
 
     @Test
     void testSentences() {
+        List<View> views = container.findViewsThatContain(Discriminators.Uri.SENTENCE)
+        assert 1 == views.size()
+        assert 1 == views[0].annotations.size()
 
+        List<Annotation> annotations = views[0].annotations
+        assertEquals 'Karen flew to New York.', extractText(annotations[0])
     }
 }
